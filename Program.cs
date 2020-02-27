b93541e1e2093ff5f7b5b88f26910df68bc38d1d
garrettfreshwater-nss <gfresh.nss@gmail.com>
2020-02-27 14:30:45 -0600

    First commit

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿//"using" import statenents. SYSTEM gives access to console
+using System;
+using System.Collections.Generic;
+
+namespace MyApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "Hello World!";
+            int numberOfTacos = 2;
+            bool hasMeat = true;
+
+            // array list
+            List<string> names = new List<string>() { "Adam", "Brenda", "Rose" };
+
+            // for loops
+            for (int i = 0; i < names.Count; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+
+            // for each
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+            // conditionals
+
+            if (numberOfTacos > 5)
+            {
+                Console.WriteLine("Oh Boy, thasalotta tacos.");
+            }
+            else if (numberOfTacos < 3)
+            {
+                Console.WriteLine("Level up your taco game");
+            }
+            else
+            {
+                Console.WriteLine("Sounds like a good number o' tacos.");
+            }
+
+
+            //string concatination
+            Console.WriteLine(message + " " + "I would like " + numberOfTacos + " Tacos. Meat? " + hasMeat);
+            //string interpolation
+            Console.WriteLine($"{message} I'd like {numberOfTacos} tacos. Meat? {hasMeat}");
+        }
+    }
+}
